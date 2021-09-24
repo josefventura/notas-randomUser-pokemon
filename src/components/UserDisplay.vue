45de9da5892b47c57c6292f28b01a1fbf40fc1ab
josef <josuelvent@gmail.com>
2021-09-23 23:38:30 -0300

    randomUser app done

--- src/components/UserDisplay.vue
+++ src/components/UserDisplay.vue
@@ -2,27 +2,26 @@
 
     <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
         <div class="card p-4">
+
             <div class=" image d-flex flex-column justify-content-center align-items-center"> 
-                <button class="btn btn-secondary"> 
-                    <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" />
+                <button class="btn btn-secondary" v-on:click="show()"> 
+                    <img v-bind:src="this.dataUser[0].picture.medium" height="100" width="100" />
                     </button> 
-                    <span class="name mt-3">Eleanor Pena</span> 
-                    <span class="idd">@eleanorpena</span>
+                    <span class="name mt-3">{{ this.dataUser[0].name.first + ' ' + this.dataUser[0].name.last }}</span> 
+                    <span class="idd">{{ '@' + this.dataUser[0].name.first+this.dataUser[0].name.last }}</span>
                 <div class="d-flex flex-row justify-content-center align-items-center gap-2"> 
-                    <span class="idd1">Oxc4c16a645_b21a</span> 
+                    <span class="idd1">{{ this.dataUser[0].location.country + ' ' + this.dataUser[0].location.state + ' ' + this.dataUser[0].location.street.name + ' ' + this.dataUser[0].location.street.number }}</span> 
                     <span><i class="fa fa-copy"></i></span> 
                 </div>
                 <div class="d-flex flex-row justify-content-center align-items-center mt-3"> 
-                    <span class="number">1069 
-                        <span class="follow">Followers</span>
+                    <span class="number"> 
+                        {{ this.dataUser[0].phone }}
                     </span> 
                 </div>
                 <div class="text mt-3"> 
-                    <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.
-                        <br/>
-                        <br/> 
-                        Artist/ Creative Director by Day #NFT minting@ with FND night. 
-                        </span> 
+                    <span>
+                        {{ this.dataUser[0].login.username }}
+                    </span> 
                 </div>
                 <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> 
                     <span><i class="fa fa-twitter"></i></span> 
@@ -31,7 +30,7 @@
                     <span><i class="fa fa-linkedin"></i></span> 
                 </div>
                 <div class=" px-2 rounded mt-4 date "> 
-                    <span class="join">Joined May,2021</span> 
+                    <span class="join">{{ this.dataUser[0].email }}</span> 
                 </div>
             </div>
         </div>
@@ -43,12 +42,31 @@
 export default ({
     name: 'UserDisplay',
     props:{
-        dataUser:{
+        dataUser:[ {
             default:{
-                name: 'no name',
+                picture:{ 
+                    thumbnail:'https://i.imgur.com/wvxPV9S.png'
+                    },
+                name:{
+                    first:'Eleanor',
+                    last:'Pena'
+                }, 
+                location:{
+                    country:'Oxc4c16a645_b21a',
+                    state:'Oxc4c16a645_b21a',
+                    street: {
+                        name:'Oxc4c16a645_b21a',
+                        number: 65465
+                    }
+                },
+                email: 'Eleanor Pena',
+                login:{
+                    username: 'Eleanor Pena'
+                },
+                phone: 890982,
 
             }
-        }
+        }]
     },
     data(){
         return{
@@ -56,7 +74,9 @@ export default ({
         }
     },
     methods:{
-
+        show(){
+            console.log(this.dataUser)
+        }
     },
 
     mounted(){
